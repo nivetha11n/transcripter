9c412511872ab19beb85d49669add79d3770f555
nivetha11n <49003463+nivetha11n@users.noreply.github.com>
2024-03-19 11:20:06 +0100

    submission of audio file

--- lib/transcripter_web/live/page_live.ex
+++ lib/transcripter_web/live/page_live.ex
@@ -7,44 +7,25 @@ defmodule TranscripterWeb.PageLive do
    {:ok, assign(socket, form: to_form(%{}))}
   end
 
- # def mount(_params, _session, socket) do
-  #  socket =
-   #   socket
-   #   |> assign(:uploaded_files, [])
-    #  |> allow_upload(:audio, accept: ~w(audio/mpeg audio/wav), max_entries: 1)
-
-   # {:ok, socket}
-  #end
-
-  #<.live_file_input upload = {@form[:audio]}/>
-
   def render(assigns) do
     ~H"""
-     <.simple_form for = {@form} phx-submit = "save_audio">
+     <.simple_form for = {@form} phx-submit = "convert_to_text">
      <.live_file_input upload={@uploads.audio} />
-     <.button type = "upload">Audio Upload</.button>
+     <.button type = "submit">convert_to_text</.button>
      </.simple_form>
     """
   end
 
-  #def render(assigns) do
-   # ~H"""
-   # <div>
-   #   <h2>Upload an Audio File</h2>
-    #  <%= f = form_for :upload, "#", phx_submit: :save_audio, multipart: true %>
-     #   <%= live_file_input f, :audio %>
-      #  <button type="submit">Upload Audio</button>
-     # </form>
-   # </div>
-  #  """
- # end
-
-  def handle_event("upload", _params, socket) do
+  def handle_event("convert_to_text", _params, socket) do
+    audio_upload = Map.get(socket.assigns.uploads, :audio)
+    if audio_upload do
+      Task.start(fn -> process_audio(audio_upload) end)
+    end
     {:noreply, socket}
   end
 
-  #def handle_event("save_audio", %{"upload_file" => the_uploaded_file}, socket) do
-   # {:noreply, socket}
-  #end
+  defp process_audio(uploaded_audio) do
+    IO.inspect(uploaded_audio, label: "Received audio file , okay bye")
+  end
 
 end
