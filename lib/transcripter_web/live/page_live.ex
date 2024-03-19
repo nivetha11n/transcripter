3adf5ed92c27692ef5b6f7ad1b7bbf1babaa851e
nivetha11n <49003463+nivetha11n@users.noreply.github.com>
2024-03-19 10:43:34 +0100

    file upload from system

--- lib/transcripter_web/live/page_live.ex
+++ lib/transcripter_web/live/page_live.ex
@@ -3,22 +3,48 @@ defmodule TranscripterWeb.PageLive do
 
 
   def mount(_params, _session, socket) do
-   {:ok, assign(socket, number: 7)}
+   socket = socket |> assign(:uploaded_file, []) |> allow_upload(:audio, accept: ["audio/mpeg", "audio/wav"], max_entries: 1)
+   {:ok, assign(socket, form: to_form(%{}))}
   end
+
+ # def mount(_params, _session, socket) do
+  #  socket =
+   #   socket
+   #   |> assign(:uploaded_files, [])
+    #  |> allow_upload(:audio, accept: ~w(audio/mpeg audio/wav), max_entries: 1)
+
+   # {:ok, socket}
+  #end
+
+  #<.live_file_input upload = {@form[:audio]}/>
 
   def render(assigns) do
     ~H"""
-    <%= assigns.number %>
-    <.button phx-click = "add">Add</.button>
+     <.simple_form for = {@form} phx-submit = "save_audio">
+     <.live_file_input upload={@uploads.audio} />
+     <.button type = "upload">Audio Upload</.button>
+     </.simple_form>
     """
   end
 
-  def handle_event("add", _params, socket) do
-    current_number = socket.assigns.number
-    new_number = current_number + 1
+  #def render(assigns) do
+   # ~H"""
+   # <div>
+   #   <h2>Upload an Audio File</h2>
+    #  <%= f = form_for :upload, "#", phx_submit: :save_audio, multipart: true %>
+     #   <%= live_file_input f, :audio %>
+      #  <button type="submit">Upload Audio</button>
+     # </form>
+   # </div>
+  #  """
+ # end
 
-    {:noreply, assign(socket, :number, new_number)}
+  def handle_event("upload", _params, socket) do
+    {:noreply, socket}
   end
 
+  #def handle_event("save_audio", %{"upload_file" => the_uploaded_file}, socket) do
+   # {:noreply, socket}
+  #end
 
 end
