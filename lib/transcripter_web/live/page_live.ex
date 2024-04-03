515cae400c2fb2f4e90d82e4c1396627b7addd76
nivetha11n <49003463+nivetha11n@users.noreply.github.com>
2024-04-03 18:13:33 +0200

    removed the file upload part

--- lib/transcripter_web/live/page_live.ex
+++ lib/transcripter_web/live/page_live.ex
@@ -4,12 +4,9 @@ defmodule TranscripterWeb.PageLive do
   def mount(_params, _session, socket) do
     socket =
     socket
-    |> assign(:transcription, nil)
-    |> assign(:uploaded_files, [])
-    |> assign(:form, to_form(%{}))
     |> assign(:recording_status,nil)
     |> assign(:last_recorded_file,nil)
-    |> allow_upload(:audio, accept: ["audio/*"], max_entries: 1, auto_upload: true)
+    |> assign(:transcription_result, nil)
 
     #|> allow_upload(:audio, accept: ["audio/mpeg", "audio/wav","audio/mp4"], max_entries: 1, auto_upload: true)
 
@@ -22,20 +19,10 @@ defmodule TranscripterWeb.PageLive do
      <div>
       <.button phx-click="start_recording">Start Recording</.button>
       <.button phx-click="stop_recording">Stop Recording</.button>
-
-     </div>
-     <div>
-     <.simple_form for = {@form}
-     id="tempfolder"
-     phx-submit = "convert_to_text"
-     phx-change="validate">
-     <.live_file_input upload={@uploads.audio} />
-     <.button type = "submit">convert to text</.button>
-     </.simple_form>
      </div>
 
-     <p><%= @transcription %></p>
      <p><%= @last_recorded_file %></p>
+      <p><%= @transcription_result %></p>
    """
   end
 
@@ -48,7 +35,10 @@ defmodule TranscripterWeb.PageLive do
         TranscripterWeb.RecordTest.record_audio_segment(output_path)
       end)
 
-      {:noreply, assign(socket, recording_status: "Recording", last_recorded_file: unique_filename)}
+      Process.sleep(10_000)
+      transcription_result = speech_to_text(output_path)
+
+      {:noreply, assign(socket, recording_status: "Recording", last_recorded_file: unique_filename, transcription_result: transcription_result)}
     end
 
   def handle_event("stop_recording", _params, socket) do
@@ -56,24 +46,6 @@ defmodule TranscripterWeb.PageLive do
     {:noreply, assign(socket, recording_status: "Not Recording")}
   end
 
-  def handle_event("convert_to_text", _params, socket) do
-
-    uploaded_files =
-    consume_uploaded_entries(socket, :audio, fn %{path: path}, _entry ->
-      dest = Path.join(Application.app_dir(:transcripter, "priv/static/uploads"), Path.basename(path))
-
-      File.cp!(path, dest)
-      speech_to_text_result = speech_to_text(path)
-      {:ok, speech_to_text_result}
-
-    end)
-   {:noreply, assign(socket, :transcription, Enum.join(uploaded_files, ", "))}
-  end
-
-
-  def handle_event("validate", _params, socket) do
-    {:noreply, socket}
-  end
 
   def speech_to_text(path) do
     {:ok, whisper} = Bumblebee.load_model({:hf, "openai/whisper-tiny"})
